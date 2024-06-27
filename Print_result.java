f795a42130a666aa7845c89ec381bd1c4ba2d161
222K6032 <f22k6032@kanto-gakuin.ac.jp>
2024-06-27 13:53:22 +0900

    replace

--- Print_result.java
+++
@@ -1,6 +0,0 @@
-public class Print_result {
-    public static void print_result(double lastYearGPA, double thisYearGPA) {
-        System.out.printf("入学してから前年度までの GPA: %.2f%n", lastYearGPA);
-        System.out.printf("入学してから今年度までの目標となる GPA: %.2f%n", thisYearGPA);
-    }
-}
--- Thisyear_unit.java
+++
@@ -1,3 +0,0 @@
-public class Thisyear_unit {
-    
-}
